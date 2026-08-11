2da05859951baa9554709962f4490c1745b37b36
Viren Patadiya <virenpatadiya073@gmail.com>
2026-08-11 10:54:34 +0000

    feat: enhance event handling and update QR code generation

--- main.go
+++ main.go
@@ -9,8 +9,10 @@ import (
 
 	"go.mau.fi/whatsmeow"
 	"go.mau.fi/whatsmeow/store/sqlstore"
+	"go.mau.fi/whatsmeow/types"
 	"go.mau.fi/whatsmeow/types/events"
 	waLog "go.mau.fi/whatsmeow/util/log"
+	qrterminal "github.com/mdp/qrterminal/v3"
 
 	_ "modernc.org/sqlite" // needed for sqlite driver
 )
@@ -18,14 +20,34 @@ import (
 func eventHandler(evt any) {
 	switch v := evt.(type) {
 	case *events.Message:
-		fmt.Println("Received a message!", v.Message.GetConversation())
+		// Status/story updates arrive as regular messages sent to the special "status" JID
+		if v.Info.Chat == types.StatusBroadcastJID {
+			caption := v.Message.GetConversation()
+			if img := v.Message.GetImageMessage(); img != nil && caption == "" {
+				caption = img.GetCaption()
+			}
+			if vid := v.Message.GetVideoMessage(); vid != nil && caption == "" {
+				caption = vid.GetCaption()
+			}
+			fmt.Println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ Status update from:", v.Info.Sender, "-", caption)
+		} else {
+			fmt.Println("---------------------------------------------------------------------------------------- Received a message!", v.Message.GetConversation())
+		}
+
+	case *events.Picture:
+		if v.Remove {
+			fmt.Println("🖼️ Picture removed for:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", v.JID, "by", v.Author)
+		} else {
+			fmt.Println("🖼️ Picture changed for:XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX", v.JID, "by", v.Author, "- new ID:", v.PictureID)
+		}
+
 	}
 }
 
 func main() {
 	dbLog := waLog.Stdout("Database", "DEBUG", true)
 	ctx := context.Background()
-	container, err := sqlstore.New(ctx, "sqlite", "file:examplestore.db?_foreign_keys=on", dbLog)
+	container, err := sqlstore.New(ctx, "sqlite", "file:examplestore.db?_foreign_keys=on&_journal_mode=WAL&_busy_timeout=5000", dbLog)
 	if err != nil {
 		panic(err)
 	}
@@ -45,7 +67,7 @@ func main() {
 		}
 		for evt := range qrChan {
 			if evt.Event == "code" {
-				fmt.Println("QR code:", evt.Code)
+				qrterminal.GenerateHalfBlock(evt.Code, qrterminal.L, os.Stdout)
 			} else {
 				fmt.Println("Login event:", evt.Event)
 			}
